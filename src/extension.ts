1df9359f8d002049cf4288cb8577e4c270834f14
Sean Heath <se@nheath.com>
2020-05-15 11:57:01 -0400

    tag completion working

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import { existsSync, readFile, writeFileSync } from "fs";
 import { basename, dirname, join, normalize, relative, resolve } from "path";
-import {
-  CancellationToken,
-  commands,
-  CompletionContext,
-  CompletionItemProvider,
-  ExtensionContext,
-  FileSystem,
-  languages,
-  Position,
-  TextDocument,
-  Uri,
-  window,
-  workspace,
-} from "vscode";
 import { getEnabledCategories } from "trace_events";
+import { downloadAndUnzipVSCode } from "vscode-test";
+import vscode = require('vscode');
 
 enum Status {
   incomplete,
@@ -28,31 +16,36 @@ const FILE_GLOB = "*.{md,markdown}";
 const LINK_REGEX = /\[\[[\w\-. ]+\]\]/i;
 
 class Graph {
-  nodes: Set<Uri>;
-  edges: Set<[Uri, Uri]>;
+  nodes: Set<vscode.Uri>;
+  edges: Set<[vscode.Uri, vscode.Uri]>;
   initialized: Boolean;
 
   constructor() {
-    this.nodes = new Set<Uri>();
-    this.edges = new Set<[Uri, Uri]>();
+    this.nodes = new Set<vscode.Uri>();
+    this.edges = new Set<[vscode.Uri, vscode.Uri]>();
     this.initialized = false;
   }
 
-  addNode(u: Uri) {
+  getNodeNames() {
+    const nodeArray = Array.from(this.nodes);
+    return nodeArray.map((node) => fname(node));
+  }
+
+  addNode(u: vscode.Uri) {
     this.nodes.add(u);
   }
 
-  addEdge(start: Uri, stop: Uri) {
+  addEdge(start: vscode.Uri, stop: vscode.Uri) {
     this.edges.add([start, stop]);
   }
 
-  addEdges(file: Uri, edges: Uri[]) {
+  addEdges(file: vscode.Uri, edges: vscode.Uri[]) {
     edges.forEach((edge) => {
       this.addEdge(file, edge);
     });
   }
 
-  getLinks(uri: Uri, i = 0) {
+  getLinks(uri: vscode.Uri, i = 0) {
     const links = Array.from(this.edges).filter((edge) => {
       return edge[i].fsPath === uri.fsPath;
     }).map((edge) => {
@@ -62,12 +55,12 @@ class Graph {
     return new Set(links);
   }
 
-  getBacklinks(u: Uri) {
+  getBacklinks(u: vscode.Uri) {
     return this.getLinks(u, 1);
   }
 }
 
-function fname(u: Uri) {
+function fname(u: vscode.Uri) {
   return basename(u.fsPath, "." + u.fsPath.split(".").pop());
 }
 
@@ -89,8 +82,8 @@ async function getFileFromLink(link: string) {
   return file[0];
 }
 
-async function getLinks(file: Uri) {
-  const contents = await workspace.fs.readFile(file);
+async function getLinks(file: vscode.Uri) {
+  const contents = await vscode.workspace.fs.readFile(file);
   const links = contents
     .toString()
     .split(/\s/)
@@ -100,12 +93,12 @@ async function getLinks(file: Uri) {
 }
 
 async function getWorkspaceFiles() {
-  return workspace.findFiles("**/" + FILE_GLOB).then((files) => {
+  return vscode.workspace.findFiles("**/" + FILE_GLOB).then((files) => {
     return files.filter((file) => file.scheme === "file");
   });
 }
 
-async function getEdges(u: Uri) {
+async function getEdges(u: vscode.Uri) {
   const links = await getLinks(u);
   const edges = await getFilesFromLinks(new Set(links));
   return edges;
@@ -117,71 +110,84 @@ async function asyncForEach(array: any, callback: any) {
   }
 }
 
-async function updateGraph() {
-  const g = new Graph();
+async function updateGraph(g: Graph) {
   const files = await getWorkspaceFiles();
-  await asyncForEach(Array.from(files), async (file: Uri) => {
+  const tg = new Graph();
+  await asyncForEach(Array.from(files), async (file: vscode.Uri) => {
     const edges = await getEdges(file);
-    g.addNode(file);
-    g.addEdges(file, edges);
+    tg.addNode(file);
+    tg.addEdges(file, edges);
 
   });
+  g.nodes = tg.nodes;
+  g.edges = tg.edges;
   g.initialized = true;
   return g;
 };
 
-/*
-class MDCompletionItemProvider implements CompletionItemProvider {
-  public async provideCompletionItems(
-    document: TextDocument,
-    position: Position,
-    _token: CancellationToken,
-    context: CompletionContext
-  ) {}
+class MDCompletionItemProvider implements vscode.CompletionItemProvider {
+  graph: Graph;
+
+  constructor(g: Graph) {
+    this.graph = g;
+  }
+
+  provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+    let linePrefix = document.lineAt(position).text.substr(0, position.character);
+    if (!linePrefix.endsWith('[[')) {
+      return undefined;
+    }
+    var names = this.graph.getNodeNames();
+    var items = names.map((name) => {
+      var kind = vscode.CompletionItemKind.File;
+      var label = name;
+      var item = new vscode.CompletionItem(label, kind);
+      return item;
+    });
+    return items;
+  }
 }
-*/
 
 function getCurrentBacklinks(g: Graph) {
-  const openFile = window.activeTextEditor?.document.uri;
+  const openFile = vscode.window.activeTextEditor?.document.uri;
   if (openFile) {
     console.debug("Checking backlinks for: " + fname(openFile));
     const backlinks = g.getBacklinks(openFile);
     return backlinks;
   }
   // return empty set if no links found
-  return new Set<Uri>();
+  return new Set<vscode.Uri>();
 }
 
 function getCurrentLinks(g: Graph) {
-  const openFile = window.activeTextEditor?.document.uri;
+  const openFile = vscode.window.activeTextEditor?.document.uri;
   if (openFile) {
     //return getLinks(openFile);
     return g.getLinks(openFile);
   }
-  return new Set<Uri>();
+  return new Set<vscode.Uri>();
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export async function activate(context: ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors
   // (console.error) This line of code will only be executed once when your
   // extension is activated
   console.log("Zettnote is now active");
-  let graph = await updateGraph();
+  var graph = new Graph();
+  graph = await updateGraph(graph);
+
   const md = { scheme: "file", language: "markdown" };
-  //init(graph);
-  /*
   context.subscriptions.push(
-    languages.registerCompletionItemProvider(md, new MDCompletionItemProvider())
+    vscode.languages.registerCompletionItemProvider(md, new MDCompletionItemProvider(graph), '[')
   );
-  */
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let getLinksCom = commands.registerCommand("zettnote.getLinks", () => getCurrentLinks(graph));
-  let getBacklinksCom = commands.registerCommand(
+  let getLinksCom = vscode.commands.registerCommand("zettnote.getLinks", () => getCurrentLinks(graph));
+  let getBacklinksCom = vscode.commands.registerCommand(
     "zettnote.getBacklinks", () => getCurrentBacklinks(graph));
 
   context.subscriptions.push(getLinksCom);
